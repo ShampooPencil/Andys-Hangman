3f760f959e315e3c52feac707483d14d06697181
Andrew Aramouni <aramouni8@gmail.com>
2023-04-15 16:55:57 -0400

    adding an account first

---
+++ andys-hangman/src/Login.js
@@ -0,0 +1,7 @@
+import LoginButton from "./LoginButton";
+
+export default function Login(){
+return(
+    <LoginButton/>
+)
+}
---
+++ andys-hangman/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import './index.css';
+import Login from './Login.js';
+import LogoutBtn from './LogoutBtn.js';
+import Profile from './Profile.js'
+
+
+export default function App(){
+  return(
+    <div>
+      <Login/>
+      <LogoutBtn/>
+      <Profile/>
+    </div>
+  )
+}
+const root = createRoot(document.querySelector("#react-root"));
+root.render(
+  <Auth0Provider
+    // domain="dev-0flopda72uhrfb0g.us.auth0.com"
+    domain="dev-0flopda72uhrfb0g.us.auth0.com"
+    clientId="IqpT21CYo4VYPDElU4v7D1gvVgye3Mx3"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: "https://dev-0flopda72uhrfb0g.us.auth0.com/api/v2/",
+      scope: "read:current_user update:current_user_metadata"
+    }}
+  >
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+  </Auth0Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
